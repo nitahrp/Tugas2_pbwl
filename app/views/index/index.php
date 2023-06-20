1f9079d254f8e9d46fd77298dbce3676ba1dd2dc
unknown <nitamaharani844@gmail.com>
2023-06-20 21:46:21 +0700

    first commit

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,3 +1,3 @@
-<h2></h2>HAI SOBAT BENGKEL</h2>
-<div class="info">Selamat datang, <strong>Bengkel Las Maju Jaya</strong></div>
+<h2></h2>HAI SOBAT VAKSIN</h2>
+<div class="info">Selamat datang, <strong>Di Vaksinasi</strong></div>
 <div class="info"><a href="login">Login</a></div>
